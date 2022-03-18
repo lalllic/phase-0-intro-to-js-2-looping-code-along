f2f649d3399f8ea920f62fc355832ff233560ddd
Isabella Costabile <costabileisa@gmail.com>
2022-03-17 20:58:54 -0400

    commit tree 19163f367d7dd22ee694ffb3d3bdb30a715573ab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(nameArray, type) {
+    let cards = [];
+    for (const name of nameArray) {
+        cards.push(`Thank you, ${name}, for the wonderful ${type} gift!`);
+    }
+    return cards;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
